5466065c015a66f4374a3a1c84596f85ae2ae30b
gynt <gynt@users.noreply.github.com>
2018-03-29 20:09:11 +0200

    cleaning up options

--- fbpy/__init__.py
+++ fbpy/__init__.py
@@ -63,6 +63,104 @@ def fetch_post(post_id, post_options=PostOptions(), reaction_options=ReactionOpt
 
     return result.content
 
+def options_to_parameters(options = {
+    
+        "limit":1,
+        "fields":{
+            "from":{},
+            "message":{},
+            "attachments":{},
+            "reactions":{
+                "limit":100,
+                "fields":{
+                    "id":{},
+                    "name":{},
+                    "type":{}
+                    }
+                },
+            "id":{},
+            "created_time":{},
+            "comments":{
+                "order":"chronological",
+                "limit":100,
+                "fields":{
+                    "reactions":{
+                        "limit":100,
+                        "fields":{
+                            "id":{},
+                            "name":{},
+                            "type":{}
+                            }
+                        },
+                    "message":{},
+                    "from":{},
+                    "id":{},
+                    "created_time":{},
+                    "comments":{
+                        "order":"chronological",
+                        "limit":100,
+                        "fields":{
+                            "reactions":{},
+                            "message":{},
+                            "from":{},
+                            "id":{},
+                            "created_time":{}
+                            }
+                        }
+                    }
+                }
+            },
+    }):
+    result = ""
+    for key in options:
+        
+    #?limit=1
+    #&fields=from,message,attachments,reactions.limit(100){id,name,type},id,created_time,comments.order(chronological).limit(100){reactions.limit(100){id,name,type},message,name,id,created_time,comments.order(chronological).limit(100){reactions,message,from,id,created_time}}
+    for key in options:
+            if not options[key]:
+                    result+="&"+key
+            elif isinstance(options[key], int):
+                    result+="{}={}".format(key, options[key])
+            elif isinstance(options[key], dict):
+                    result+="&{}=".format(key)
+                    suboptions = options[key]
+                    for key in suboptions:
+                            if not suboptions[key]:
+                                    result+=",{}".format(key)
+    pass
+
+def fetch_object(obj_id, options={}):
+    pass
+
+def extract(obj, key, limit=pow(2,31), recurse = True):
+
+    count = 0
+
+    page = obj[key]
+
+    if not "data" in page:
+        raise Exception("Not a paging type")
+
+    while count < limit:
+
+        for item in page["data"]:
+            if recurse:
+                if isinstance(item, dict):
+                    if key in item.keys():
+                        extract(item, key, limit, recurse)
+            count+=1
+            yield item
+
+            if count > limit - 1:
+                return
+
+        p = Page(page)
+        if p.has_next():
+            page = p.next(access_manager).page
+        else:
+            debug("No more pages")
+            break
+
 def extract_comments_for_post(post, amount=99999999, include_subcomments=True):
 
     count = 0
